5694f4ffd7b5ba30a708921dfef2d96a000ea15b
Rameez Adil <rameezadil93@gmail.com>
2024-10-16 10:48:53 +0800

    added fetch, switch and LDA.

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ typedef unsigned int u32;
 
 #define MAX_MEM (1024 * 64)
 
+#define INS_LDA_IM 0xA9
+
 struct Mem {
   Byte Data[MAX_MEM];
 };
@@ -42,25 +44,47 @@ void Reset(struct CPU *cpu, struct Mem memory) {
   Initialise(&memory);
 }
 
-void Execute(u32 Cycles, struct CPU cpu, struct Mem memory) {
-  printf("Program counter: %X\n", cpu.PC);
-  printf("Stack pointer: %X\n", cpu.S);
-
-  /*for (u32 i = 0; i < MAX_MEM; i++) {
-    printf("%X", memory.Data[i]);
-  } */
+Byte Fetch(u32 *Cycles, struct CPU *cpu, struct Mem memory) {
+  Byte Data = memory.Data[cpu->PC];
+  cpu->PC++;
+  (*Cycles)--;
+  return Data;
 }
+
+void Execute(u32 Cycles, struct CPU cpu, struct Mem memory) {
+  while (Cycles > 0) {
+    Byte Instruction = Fetch(&Cycles, &cpu, memory);
+    switch (Instruction) {
+    case INS_LDA_IM: {
+      Byte Value = Fetch(&Cycles, &cpu, memory);
+      cpu.A = Value;
+      cpu.Z = (cpu.A == 0);
+      cpu.N = (cpu.A & 0b10000000) > 0;
+      printf("%X\n", cpu.PC);
+      printf("%X\n", cpu.A);
+      printf("%X\n", cpu.Z);
+      printf("%X\n", cpu.N);
+    } break;
+    default: {
+      printf("instruction not handled %X\n", cpu.PC);
+    } break;
+    }
+  }
+}
+
 int main() {
 
   struct CPU cpu;
   struct Mem *mem =
       malloc(sizeof(struct Mem)); // allocate memory for the struct
   Reset(&cpu, *mem);
-  Execute(1, cpu, *mem);
 
-  /*for (u32 i = 0; i < MAX_MEM; i++) {
-    printf("%X", mem->Data[i]);
-  }*/
+  mem->Data[0xFFFC] = 0xA9;
+  mem->Data[0xFFFD] = 0x00; // indicate all 0 flag Z
+  mem->Data[0xFFFE] = 0xA9;
+  mem->Data[0xFFFF] = 0x80; // set negative flag
+
+  Execute(4, cpu, *mem);
 
   return 0;
 }
