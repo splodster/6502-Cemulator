65dca49e0d683fe05b1df43b9942686b1d4335bc
Rameez Adil <rameezadil93@gmail.com>
2024-10-17 19:55:56 +0800

    Added JSR

--- main.c
+++ main.c
@@ -5,9 +5,12 @@ typedef unsigned char Byte;
 typedef unsigned short Word;
 typedef unsigned int u32;
 
-#define MAX_MEM (1024 * 64)
+#define MAX_MEM (1024 * 64) // 64 kb
 
-#define INS_LDA_IM 0xA9
+#define INS_JSR_A 0x20   // JUMP TO SUBROUTINE ABSOLUTE
+#define INS_LDA_IM 0xA9  // LOAD ACCUMULATOR IMMEDIATE
+#define INS_LDA_ZP 0xA5  // ZERO PAGE
+#define INS_LDA_ZPX 0xB5 // ZEROPAGE X
 
 struct Mem {
   Byte Data[MAX_MEM];
@@ -51,22 +54,61 @@ Byte Fetch(u32 *Cycles, struct CPU *cpu, struct Mem memory) {
   return Data;
 }
 
-void Execute(u32 Cycles, struct CPU cpu, struct Mem memory) {
+Word FetchW(u32 *Cycles, struct CPU *cpu, struct Mem memory) {
+  Word Data = memory.Data[cpu->PC];
+  cpu->PC++;
+  Data |= (memory.Data[cpu->PC] << 8);
+  cpu->PC++;
+  (*Cycles) -= 2;
+  return Data;
+}
+
+Byte Read(Byte Address, u32 *Cycles, struct CPU cpu, struct Mem memory) {
+  Byte Data = memory.Data[Address];
+  (*Cycles)--;
+  return Data;
+}
+
+void WriteW(u32 *Cycles, struct Mem *memory, Word Value, u32 Address) {
+  memory->Data[Address] = Value & 0xFF;
+  memory->Data[Address + 1] = (Value >> 8);
+  (*Cycles) -= 2;
+}
+
+void SetLDAStatusFlag(struct CPU *cpu) {
+  cpu->Z = (cpu->A == 0);
+  cpu->N = (cpu->A & 0b10000000) > 0;
+}
+
+void Execute(u32 Cycles, struct CPU *cpu, struct Mem memory) {
   while (Cycles > 0) {
-    Byte Instruction = Fetch(&Cycles, &cpu, memory);
+    Byte Instruction = Fetch(&Cycles, cpu, memory);
     switch (Instruction) {
     case INS_LDA_IM: {
-      Byte Value = Fetch(&Cycles, &cpu, memory);
-      cpu.A = Value;
-      cpu.Z = (cpu.A == 0);
-      cpu.N = (cpu.A & 0b10000000) > 0;
-      printf("%X\n", cpu.PC);
-      printf("%X\n", cpu.A);
-      printf("%X\n", cpu.Z);
-      printf("%X\n", cpu.N);
+      Byte Value = Fetch(&Cycles, cpu, memory);
+      cpu->A = Value;
+      SetLDAStatusFlag(cpu);
+    } break;
+    case INS_LDA_ZP: {
+      Byte ZeroPageAddress = Fetch(&Cycles, cpu, memory);
+      cpu->A = Read(ZeroPageAddress, &Cycles, *cpu, memory);
+      SetLDAStatusFlag(cpu);
+    } break;
+    case INS_LDA_ZPX: {
+      Byte ZeroPageAddress = Fetch(&Cycles, cpu, memory);
+      Cycles--;
+      ZeroPageAddress += cpu->X;
+      cpu->A = Read(ZeroPageAddress, &Cycles, *cpu, memory);
+      SetLDAStatusFlag(cpu);
+    } break;
+    case INS_JSR_A: {
+      Word SubroutineAddress = FetchW(&Cycles, cpu, memory);
+      WriteW(&Cycles, &memory, (cpu->PC - 1), cpu->S);
+      cpu->PC = SubroutineAddress;
+      Cycles--;
     } break;
     default: {
-      printf("instruction not handled %X\n", cpu.PC);
+      printf("instruction not handled %X\n", cpu->PC);
     } break;
     }
   }
@@ -79,12 +121,15 @@ int main() {
       malloc(sizeof(struct Mem)); // allocate memory for the struct
   Reset(&cpu, *mem);
 
-  mem->Data[0xFFFC] = 0xA9;
-  mem->Data[0xFFFD] = 0x00; // indicate all 0 flag Z
-  mem->Data[0xFFFE] = 0xA9;
-  mem->Data[0xFFFF] = 0x80; // set negative flag
+  mem->Data[0xFFFC] = INS_JSR_A;
+  mem->Data[0xFFFD] = 0x42;
+  mem->Data[0xFFFE] = 0x42;
+  mem->Data[0x4242] = INS_LDA_IM;
+  mem->Data[0x4243] = 0x84;
 
-  Execute(4, cpu, *mem);
+  Execute(8, &cpu, *mem);
+
+  printf("%X", cpu.A);
 
   return 0;
 }
